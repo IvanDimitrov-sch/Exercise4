befe57bf59d3aa1ac92350e2fccb28c9f3f8b9f3
ElectroDragon777 <viki.mladost@gmail.com>
2025-04-14 18:03:11 +0300

    Classes done

---
+++ 14-0425/FileRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_0425
+{
+    internal class FileRequest
+    {
+        
+        public string Name { get; set; }
+        //public string Description { get; set; }
+        public FileRequest(string name, string description)
+        {
+            Name = name;
+            //Description = description;
+        }
+    }
+}
---
+++ 14-0425/RequestManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_0425
+{
+    internal class RequestManager
+    {
+        public List<FileRequest> Requests = new List<FileRequest>();
+        
+        public RequestManager()
+        {
+
+        }
+        public void Add(FileRequest req)
+        {
+            if (!Requests.Contains(req))
+            {
+                Requests.Add(req);  
+            }
+            
+        }
+    }
+}
